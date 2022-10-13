db52253b2a5a468792140f884d9e308c5dfa8e06
vision <karohambaryan@gmail.com>
2022-10-14 00:10:11 +0400

    dowunloder swiper

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
 import testimonial from "./testimonials.module.css";
-import TestimonialsData from './TestimonialsData';
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Data } from './TestimonialsData';
+// import required modules
+import { Pagination } from "swiper";
 
 //   <div className={`${testimonial.testimonial} container section`}></div>
 //   <div className={testimonial.testimonial}></div>
@@ -12,7 +20,37 @@ const Testimonials = () => {
 			<h2 className="section__title">My clients say</h2>
 			<span className="section__subtitle">Testimonial</span>
 
-			<div className={testimonial.container}></div>
+			<Swiper
+				loop={true}
+				grabCursor={true}
+				spaceBetween={24}
+				pagination={{
+					clickable: true,
+				}}
+				breakpoints={{
+					576: {
+						slidesPerView: 2,
+					},
+					768: {
+						slidesPerView: 2,
+						spaceBetween: 48,
+					},
+				}}
+				modules={[Pagination]}
+				className={testimonial.container}
+			>
+				{
+					Data.map(({ id, image, title, description }) => {
+						return (
+							<SwiperSlide key={id} className={testimonial._card}>
+								<img src={image} alt="img" className={testimonial._img} />
+								<h3 className={testimonial._name}>{title}</h3>
+								<p className={testimonial._description}>{description}</p>
+							</SwiperSlide>
+						);
+					})
+				}
+			</Swiper>
 		</section>
 	);
 };
